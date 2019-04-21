c49101d43d35eb04120a94f79c435786baba41ff
RatofWrath <T_mullen93@yahoo.com>
2019-04-21 16:23:56 -0500

    created html for the home page.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,62 +1,62 @@
 var friends = [
     {
-        "name": "",
-        "photo": "",
-        "scores": []
+        "name": "Bentley Chapman",
+        "photo": "https://en.wiktionary.org/wiki/cat#/media/File:Cat03.jpg",
+        "scores": [4,3,4,4,4,5,4,1,2,5]
     },
 
     {
-        "name": "",
-        "photo": "",
-        "scores": []
+        "name": "Pearce Kent",
+        "photo": "https://www.rspcasa.org.au//wp-content/uploads/2019/01/Adopt-a-cat-or-kitten-from-RSPCA.jpg",
+        "scores": [3,5,2,1,1,4,3,5,1,1]
     },
 
     {
-        "name": "",
-        "photo": "",
-        "scores": []
+        "name": "Santiago Cole",
+        "photo": "http://danglingmouse.com/wp-content/uploads/2014/08/54.jpg",
+        "scores": [3,3,1,4,1,4,2,2,3,1]
     },
 
     {
-        "name": "",
-        "photo": "",
-        "scores": []
+        "name": "Laith Blackmore",
+        "photo": "http://danglingmouse.com/wp-content/uploads/2014/08/201.jpg",
+        "scores": [2,5,2,1,5,3,5,2,4,3]
     },
 
     {
-        "name": "",
-        "photo": "",
-        "scores": []
+        "name": "Isabella Kendall",
+        "photo": "https://usercontent2.hubstatic.com/13785369.jpg",
+        "scores": [1,2,2,1,5,3,2,3,3,3]
     },
 
     {
-        "name": "",
-        "photo": "",
-        "scores": []
+        "name": "Avaya Abbott",
+        "photo": "http://www.geocities.ws/lovecat/grfgf.jpeg",
+        "scores": [3,1,3,3,4,4,4,5,1,1]
     },
 
     {
-        "name": "",
-        "photo": "",
-        "scores": []
+        "name": "Ethan Watt",
+        "photo": "https://www.dailydot.com/wp-content/uploads/2018/10/olli-the-polite-cat.jpg",
+        "scores": [4,1,4,3,4,3,5,2,5,2]
     },
 
     {
-        "name": "",
-        "photo": "",
-        "scores": []
+        "name": "Lacey-May Edmonds",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1rCF-ozeXU4oLJjY3N8845U_Cm4rHWOHyNwQhQqPH7j2dYbjxfg",
+        "scores": [1,2,3,2,1,1,5,5,3,3]
     },
 
     {
-        "name": "",
-        "photo": "",
-        "scores": []
+        "name": "Lochlan Vance",
+        "photo": "http://eskipaper.com/images/funny-cat-expression-1.jpg",
+        "scores": [2,3,3,4,3,1,1,3,4,3]
     },
 
     {
-        "name": "",
-        "photo": "",
-        "scores": []
+        "name": "Alexandra Clements",
+        "photo": "https://sadanduseless.b-cdn.net/wp-content/uploads/2018/04/funny-face-cat.jpg",
+        "scores": [5,1,5,2,3,3,1,1,5,1]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,7 @@ modules.exports = function(app){
     });
 
     app.post("/api/friends", function(req, res){
-
+        friends.push(req.body);
+        res.json(true);
     });
 };
